fceeb75d4d28156b4b634f067290901172509f4b
maxrad02 <maxrad02@gmail.com>
2024-03-02 16:07:00 -0600

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Dashboard from './Dashboard.js';
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Dashboard/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
